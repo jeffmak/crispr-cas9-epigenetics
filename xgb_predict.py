ea3f300185d21bf9c70395dfc9ac3f552dd7c227
jeffmak <jeffrey.kelvin.mak@cs.ox.ac.uk>
2022-08-30 18:10:33 +0100

    added xgb_perturb.py and commented code

--- xgb_predict.py
+++ xgb_predict.py
@@ -1,8 +1,29 @@
-X_df = pd.read_csv('/content/drive/MyDrive/nuc_occup/sample_script/data/explainset_xgboost_noseq_engepi_nuc_MNase.csv')
-new_xgb_loc = '/content/drive/MyDrive/nuc_occup/sample_script/models/xgb_noseq_engnucepi_model_post_xgb1.6.json'
-new_xgb_model = xgb.XGBRegressor()
-new_xgb_model.load_model(new_xgb_loc)
-preds = new_xgb_model.predict(X_df)
-out_loc = '/content/drive/MyDrive/nuc_occup/sample_script/data/xgb_preds.csv'
+import pandas as pd
+import xgboost as xgb
+
+# This Python script uses the following data and model files:
+# 1. An input CSV file containing 2000 random experimental/augmented datapoints from the crisprSQL dataset in the paper
+csv_loc = 'data/explainset_xgboost_noseq_engepi_nuc_MNase.csv'
+# 2. The XGBoost tree model used for making CRISPR-Cas9 activity prediction
+xgb_loc = 'models/xgb_noseq_engnucepi_model_post_xgb1.6.json'
+
+# We will save the predictions in the "out" folder
+out_loc = 'out/xgb_preds.csv'
+
+# Create and load the XGBoost model
+# Use XGBRegressor since CRISPR-Cas9 activity values are continuous
+xgb_model = xgb.XGBRegressor()
+xgb_model.load_model(xgb_loc)
+
+# Read the input CSV file
+X_df = pd.read_csv(csv_loc)
+
+# Make predictions
+preds = xgb_model.predict(X_df)
+
+# Save the predictions
 pd.DataFrame(preds).to_csv(out_loc, index=None)
+
+# Print results
 print('Predicted CRISPR-Cas9 Activities: ' + str(preds))
+print('Read the full list of predictions at ' + out_loc)
--- xgb_predict_one.py
+++ xgb_predict_one.py
@@ -1,24 +1,25 @@
-import xgboost as xgb
 import pandas as pd
 import xgboost as xgb
-import os
 
-dirname = os.path.dirname(__file__)
-
-# This Python script uses the following files:
-
-# 1. The CSV file which we read the input from
-example_loc = os.path.join(dirname, 'data/example_input.csv')
-
-# 2. The XGBoost model
-xgb_loc = os.path.join(dirname, 'models/xgb_noseq_engnucepi_model_post_xgb1.6.json')
+# This Python script uses the following data and model files:
+# 1. An example input CSV file with one row, namely one datapoint
+# from the crisprSQL dataset in the paper
+example_loc = 'data/example_input.csv'
+# 2. The XGBoost tree model used for making CRISPR-Cas9 activity prediction
+xgb_loc = 'models/xgb_noseq_engnucepi_model_post_xgb1.6.json'
 
 
-# Make
+# Create and load the XGBoost model
+# Use XGBRegressor since CRISPR-Cas9 activity values are continuous
 xgb_model = xgb.XGBRegressor()
 xgb_model.load_model(xgb_loc)
+
+# Read the input CSV file
 example_df = pd.read_csv(example_loc)
+
+# Make the prediction
 pred = xgb_model.predict(example_df.astype('float'))
 
+# Print the result
 # -3.440 is the predicted CRISPR-Cas9 activity value
 print('Predicted CRISPR-Cas9 Activity: {:.3f}'.format(pred[0]))
